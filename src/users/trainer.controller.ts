0dd6ba348444a2ea12279331cd7268155db6f311
mamedbekovruslan <mamedbekovruslan@yandex.ru>
2026-01-13 11:45:23 +0300

    feat: Метод для получения данных по тренеру

--- src/users/trainer.controller.ts
+++ src/users/trainer.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, HttpCode, HttpStatus, ConflictException, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpCode, HttpStatus, ConflictException, BadRequestException, UseGuards, Request, Param } from '@nestjs/common';
 import { TrainerService } from './trainer.service';
 import type { CreateTrainerDto } from './trainer.service';
 import { Trainer } from './trainer.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('trainers')
 export class TrainerController {
@@ -27,5 +28,34 @@ export class TrainerController {
     }
 
     return await this.trainerService.create(createTrainerDto);
+  }
+
+  @Get('profile')
+  @UseGuards(JwtAuthGuard)
+  async getProfile(@Request() req): Promise<Trainer> {
+    console.log('Requesting profile for user:', req.user); // Логируем информацию о пользователе
+
+    // Убедимся, что пользователь является тренером
+    if (req.user.user_type !== 'trainer') {
+      throw new BadRequestException('Only trainers can access their profile');
+    }
+
+    const trainer = await this.trainerService.findById(req.user.sub);
+    if (!trainer) {
+      throw new BadRequestException('Trainer not found');
+    }
+    console.log('Returning trainer:', trainer); // Логируем возвращаемого тренера
+    return trainer;
+  }
+
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  async findOne(@Request() req, @Param('id') id: string): Promise<Trainer> {
+    const trainerId = parseInt(id, 10);
+    const trainer = await this.trainerService.findById(trainerId);
+    if (!trainer) {
+      throw new BadRequestException('Trainer not found');
+    }
+    return trainer;
   }
 }
