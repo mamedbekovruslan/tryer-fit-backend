0c151429e95f0ed27a98e5cb436bdc7351aa9633
mamedbekovruslan <mamedbekovruslan@yandex.ru>
2026-03-10 18:17:44 +0300

    feat: Добавил лимит на фотки

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
