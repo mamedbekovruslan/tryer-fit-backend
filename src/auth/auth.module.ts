c0986a4ddf819cbbecbd0133101be3480728fc7f
mamedbekovruslan <mamedbekovruslan@yandex.ru>
2026-03-11 15:48:29 +0300

    fix: Убрал тестовую ручку

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { TestAuthController } from './test-auth.controller';
 import { ClientModule } from '../users/client.module';
 import { TrainerModule } from '../users/trainer.module';
 import { JwtStrategy } from './jwt.strategy';
@@ -22,7 +21,7 @@ import { Trainer } from '../users/trainer.entity';
       signOptions: { expiresIn: process.env.JWT_EXPIRES_IN ? parseInt(process.env.JWT_EXPIRES_IN, 10) || 3600 : 3600 },
     }),
   ],
-  controllers: [AuthController, TestAuthController],
+  controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
